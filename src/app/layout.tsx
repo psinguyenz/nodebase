7db5fdfcdfe50e23cad84adcc21df055b2684002
Hoahong2002 <psinguyenz@gmail.com>
2026-01-31 23:52:43 +0700

    03: trpc setup and examples to desmonstate the power of prefetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* this allow TRPC to wrap the app and provide the tRPC context to all components */}
+        <TRPCReactProvider>
+          {children}
+        </TRPCReactProvider>
       </body>
     </html>
   );
---
+++ src/trpc/example/server-to-client/client.tsx
@@ -0,0 +1,9 @@
+// "use client";
+
+// export const Client = ({ users }: { users: Record<string, any>[] }) => {
+//     return (
+//         <div>
+//             Client component! {JSON.stringify(users)}
+//         </div>
+//     )
+// };
